50d98af7c0ce17e6df15431fcffe5c8ec1ba2648
Nhan <nguyenngocbaonhan2003@gmail.com>
2022-11-08 14:09:31 +0700

    add update ver_05 for manage rooms module

--- main/ver_05/HotelMgnt.h
+++ main/ver_05/HotelMgnt.h
@@ -3,12 +3,12 @@
 class HotelMgnt : public Room
 {
 public:
-	void guestSummaryReport(Room*, int);
-    void checkIn(Room*, int);
-	void getAvailRoom(Room*, int);
-	void searchCustomer(Room*, int, string);
-	void checkOut(Room*, int, int);
-	friend void manageRooms(Room*, int);
-	friend void getRoomsData(const string, Room*, int);
-	friend void saveRoomsData(const string, Room*, int);
+	void guestSummaryReport(Room*, int&);
+    void checkIn(Room*, int&);
+	void getAvailRoom(Room*, int&);
+	void searchCustomer(Room*, int&, string);
+	void checkOut(Room*, int&, int);
+	friend void manageRooms(Room*, int&);
+	friend void getRoomsData(const string, Room*, int&);
+	friend void saveRoomsData(const string, Room*, int&);
 };
