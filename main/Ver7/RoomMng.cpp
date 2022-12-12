ab030480f738ef49ded57bd62c7e12dc9007ca70
NamCrystal8 <sdgm2003@gmail.com>
2022-12-13 01:34:50 +0700

    Print theo doi tuong

--- main/Ver7/RoomMng.cpp
+++ main/Ver7/RoomMng.cpp
@@ -43,7 +43,8 @@ void RoomMng::printAll()
              << *(this->manage[i]);
     drawaline();
 }
-void RoomMng::printRoom(int index){
+void RoomMng::printRoom(int index)
+{
     cout << endl
          << "Phong can tim la: " << *(this->manage[index]);
 }
@@ -104,18 +105,21 @@ void RoomMng::updateRoom(int index)
     }
 }
 int RoomMng::interpolationSearch(int x)
-{   
-    if ((*this->manage[0]).getRoomNumber() == x) return 0;
+{
+    if ((*this->manage[0]).getRoomNumber() == x)
+        return 0;
     int lo = 0, hi = (this->manage.size());
     while (lo <= hi && x >= (*this->manage[lo]).getRoomNumber() && x <= (*this->manage[hi]).getRoomNumber())
     {
         if (lo == hi)
         {
-            if ((*this->manage[lo]).getRoomNumber() == x) return lo;
+            if ((*this->manage[lo]).getRoomNumber() == x)
+                return lo;
             return -1;
         }
         int pos = lo + (((double)(hi - lo) /
-            ((*this->manage[hi]).getRoomNumber() - (*this->manage[lo]).getRoomNumber())) * (x - (*this->manage[lo]).getRoomNumber()));
+                         ((*this->manage[hi]).getRoomNumber() - (*this->manage[lo]).getRoomNumber())) *
+                        (x - (*this->manage[lo]).getRoomNumber()));
         if ((*this->manage[pos]).getRoomNumber() == x)
             return pos;
         if ((*this->manage[pos]).getRoomNumber() < x)
@@ -123,11 +127,13 @@ int RoomMng::interpolationSearch(int x)
         else
             hi = pos - 1;
     }
-    return -1;}
+    return -1;
+}
 
-    void RoomMng::printBy(){
+void RoomMng::printBy()
+{
     int s;
-    cout << "\t\tIn Theo:"<< endl;
+    cout << "\t\tIn Theo:" << endl;
     cout << "\n\t1. Trang thai phong dang trong";
     cout << "\n\t2. Tien thue phong";
     cout << "\n\t3. Loai phong";
@@ -136,33 +142,43 @@ int RoomMng::interpolationSearch(int x)
     cout << endl;
     switch (s)
     {
-    case 1:{
+    case 1:
+    {
         this->printAvailable();
         break;
     }
     case 2:
     {
-        int s2; double d;
+        int s2;
+        double d;
         cout << "\n\t1. So tien be hon";
         cout << "\n\t2. So tien lon hon";
         cout << "\n\tNhap lua chon cua ban: ";
+        cin.ignore();
         cin >> s2;
         cout << "\n\tNhap so tien: ";
+        cin >> d;
         cout << endl;
         switch (s2)
         {
         case 1:
-        {   
+        {
+            drawaline();
+            cout << endl;
             for (int i = 0; i < this->manage.size(); i++)
                 if ((*this->manage[i]).getRent() < d)
-                    cout << (*this->manage[i])<< endl;           
+                    cout << (*this->manage[i]) << endl;
+            drawaline();
             break;
         }
         case 2:
         {
+            drawaline();
+            cout << endl;
             for (int i = 0; i < this->manage.size(); i++)
                 if ((*this->manage[i]).getRent() > d)
                     cout << (*this->manage[i]) << endl;
+            drawaline();
             break;
         }
         default:
@@ -171,26 +187,30 @@ int RoomMng::interpolationSearch(int x)
             break;
         }
         }
+        break;
     }
     case 3:
     {
-        int s3; bool b;
+        int s3;
+        bool b;
         cout << "\n\t1. Phong don/doi";
         cout << "\n\t2. Phong gia dinh";
         cout << "\n\tNhap loai phong: ";
         cin >> s3;
         cout << endl;
-        if (s3 == 1 )
+        if (s3 == 1)
             b = false;
         else if (s3 == 2)
             b = true;
-        else{
+        else
+        {
             this->printAll();
             break;
-        }   
-        for ( int i = 0; i < this->manage.size(); i++ )
+        }
+        for (int i = 0; i < this->manage.size(); i++)
             if ((*this->manage[i]).getType() == b)
                 cout << (*this->manage[i]);
+        break;
     }
     default:
     {
--- main/Ver7/main.cpp
+++ main/Ver7/main.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 int main(){
     Room c(1,1,1,1);
-    {Customer *b = new Customer("Nigeria","Niggeria","0101000101010","1/1/1","2/2/2",22020,1234231);
-     c.addCust(b);}
-    Customer *a = new Customer("Namdz","Niggeria","0101000101010","1/1/1","2/2/2",22020,1234231);
+    Room c1(0,0,0,0);
+    Room c2(1,199,1,1);
+
 
     Room d;
     RoomMng Main;
     Main.add(&c);
-    int vitri = Main.interpolationSearch(0);
-    if (vitri == -1) cout << "Ko tim dc vi tri phong";
-    else Main.printRoom(vitri);
+    Main.add(&c1);
+    Main.add(&c2);
+    Main.printBy();
 
 
 }
